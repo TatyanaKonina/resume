8bf1df2f80c5d10c11048c48f8f68caa4483307f
TatyanaKonina <tdkonina@edu.hse.ru>
2021-07-23 10:07:20 +0300

    fix bag

--- course/pulse/src/js/script.js
+++ course/pulse/src/js/script.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
                 }
             }
         ],
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg" /></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg" /></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg" /></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg" /></button>',
     });
 });
