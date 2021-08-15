da7d92674df92fa858d3adf8e8c8bce2e004710f
TatyanaKonina <tdkonina@edu.hse.ru>
2021-08-15 09:20:54 +0300

    =

--- course_js/basic/js/script.js
+++ course_js/basic/js/script.js
@@ -1,16 +1,36 @@
 'use strict';
-const numberOfilms = prompt('Сколько фильмов вы уже посмотрели?','');
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
-    count: numberOfilms,
+    count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
-    privat: false,
+    privat: false
 };
 
-const lastFilm = prompt('Один из последних просмотренных фильмов?', ''), 
-       mark= prompt('На сколько оцените его?','');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[lastFilm] = mark;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
 
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
+
+console.log(personalMovieDB);
