26e1d615545205159273ee39fd3ac607c59625ab
TatyanaKonina <tdkonina@edu.hse.ru>
2021-08-29 11:04:36 +0300

    refactoring

--- vue_project/medium_clone/src/views/Register.vue
+++ vue_project/medium_clone/src/views/Register.vue
@@ -48,6 +48,7 @@
 
 <script>
 import McvValidationErrors from '@/components/ValidationErrors'
+import {actionTypes} from '@/store/modules/auth'
 export default {
   name: 'McvRegister',
   components: {
@@ -71,7 +72,7 @@ export default {
   methods: {
     onSubmit() {
       this.$store
-        .dispatch('register', {
+        .dispatch(actionTypes.register, {
           email: this.email,
           username: this.username,
           password: this.password
