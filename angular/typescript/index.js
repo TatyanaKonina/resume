476466574088657bcdbd7d031adf5e669081eeb0
TatyanaKonina <tdkonina@edu.hse.ru>
2021-09-29 12:09:59 +0300

    func

--- angular/typescript/index.js
+++ angular/typescript/index.js
@@ -3,3 +3,7 @@ var num = 42;
 var isActive = false;
 var stdArr = ['h', 'e', 'l', 'l'];
 var numArr = [1, 2, 3];
+function logInfo(name, age) {
+    console.log("info : " + name + " " + age);
+}
+logInfo('Tatyana', 20);
--- angular/typescript/index.ts
+++ angular/typescript/index.ts
@@ -1,6 +1,6 @@
-{
 
-    let str:string = 'hello'
+
+let str:string = 'hello'
 let num:number = 42
 let isActive: boolean = false
 
@@ -9,4 +9,14 @@ let numArr: Array<number> = [1,2,3]
 
 
 
+function logInfo(name: string,age: number): void{
+    console.log(`info : ${name} ${age}`)
 }
+logInfo('Tatyana',20)
+
+function calc ( a:number,b:number | string):number{
+    return a+b
+}
+console.log(calc(2,3))
+
+
