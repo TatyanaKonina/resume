990013043625c017ea38a31d15e8b75187c39740
TatyanaKonina <tdkonina@edu.hse.ru>
2021-09-29 14:43:24 +0300

    template and style

---
+++ angular/angular-basics/src/app/post3/post3.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-post3',
+    template: `
+    <div class='post3'>
+        <h2> POST 3</h2>
+    </div>
+    `,
+    styles:[`
+    .post3
+        color: green
+    `]
+})
+export class Post3Component{
+
+}
