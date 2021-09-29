169b4a1577b73d0dde5d33f648fcdbf3a7e16425
TatyanaKonina <tdkonina@edu.hse.ru>
2021-09-29 12:04:00 +0300

    types basic

---
+++ angular/typescript/index.js
@@ -0,0 +1,5 @@
+var str = 'hello';
+var num = 42;
+var isActive = false;
+var stdArr = ['h', 'e', 'l', 'l'];
+var numArr = [1, 2, 3];
---
+++ angular/typescript/index.ts
@@ -0,0 +1,12 @@
+{
+
+    let str:string = 'hello'
+let num:number = 42
+let isActive: boolean = false
+
+let stdArr:string[] = ['h','e','l','l']
+let numArr: Array<number> = [1,2,3]
+
+
+
+}
