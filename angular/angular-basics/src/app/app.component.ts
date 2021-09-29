38f2dcb7761ff5e9d0e749de7d6f8388ff9656f4
TatyanaKonina <tdkonina@edu.hse.ru>
2021-09-29 14:56:42 +0300

    binding

--- angular/angular-basics/src/app/app.component.ts
+++ angular/angular-basics/src/app/app.component.ts
@@ -6,6 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'DYNAMIC TITLE'
-  arr=[1,2]
+   title = 'DYNAMIC TITLE'
+  img = 'https://appfort.ru/images/react.svg'
+  constructor(){
+    setTimeout(()=> {
+      this.img = 'https://avatars.mds.yandex.net/i?id=75f389b1e4e4dedf1f921083428a04c5-5165489-images-thumbs&n=13'
+    },5000)
+  }
+ 
 }
