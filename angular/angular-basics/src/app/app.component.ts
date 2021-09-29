71099caa8d3f08b2418ce7a7228e94ab63e54104
TatyanaKonina <tdkonina@edu.hse.ru>
2021-09-29 21:43:31 +0300

    pipe

--- angular/angular-basics/src/app/app.component.ts
+++ angular/angular-basics/src/app/app.component.ts
@@ -5,19 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
+
+
+
 export class AppComponent {
-  
- arr=[1,1,2,3,5,8,13]
- obj=[
-   {title:'POst1',author:'me',comments:[
-     {name:'angular',text:'lorem'},
-     {name:'angular',text:'lorem2'},
-     {name:'angular',text:'lorem3'},
-   ]},
-   {title:'POst2',author:'another',comments:[
-    {name:'react',text:'lorem'},
-    {name:'react',text:'lorem2'},
-    {name:'react',text:'lorem3'},
-  ]}
- ]
+  now:Date = new Date()
 }
