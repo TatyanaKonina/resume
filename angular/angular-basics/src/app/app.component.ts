7253966f54d2ec9cdbc54fa48b0332d83b78fd86
TatyanaKonina <tdkonina@edu.hse.ru>
2021-09-29 21:40:02 +0300

    ngfor

--- angular/angular-basics/src/app/app.component.ts
+++ angular/angular-basics/src/app/app.component.ts
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
- backToggle = false
+ arr=[1,1,2,3,5,8,13]
+ obj=[
+   {title:'POst1',author:'me',comments:[
+     {name:'angular',text:'lorem'},
+     {name:'angular',text:'lorem2'},
+     {name:'angular',text:'lorem3'},
+   ]},
+   {title:'POst2',author:'another',comments:[
+    {name:'react',text:'lorem'},
+    {name:'react',text:'lorem2'},
+    {name:'react',text:'lorem3'},
+  ]}
+ ]
 }
