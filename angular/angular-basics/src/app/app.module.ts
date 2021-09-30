560733efd98be136a3dd6f8c916cd1347e0ae0a4
TatyanaKonina <tdkonina@edu.hse.ru>
2021-09-30 11:40:10 +0300

    Получение доступа до элемента по directive

--- angular/angular-basics/src/app/app.module.ts
+++ angular/angular-basics/src/app/app.module.ts
@@ -4,15 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { StyleDirective } from './directives/style.directive';
-import { Style2Directive } from './style2.directive';
-import { Style3Directive } from './directives/style3.directive';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     StyleDirective,
-    Style2Directive,
-    Style3Directive
+
 
   ],
   imports: [
--- angular/angular-basics/src/app/directives/style.directive.ts
+++ angular/angular-basics/src/app/directives/style.directive.ts
@@ -1,8 +1,11 @@
-import { Directive } from "@angular/core";
+import { Directive, ElementRef, Renderer2 } from "@angular/core";
 
 @Directive({selector:'[appStyle]'})
 
 
 export class StyleDirective {
-
+    constructor(private elRef: ElementRef,private render : Renderer2){
+        // elRef.nativeElement.style.color = 'red'
+        this.render.setStyle(elRef.nativeElement,'color','blue')
+    }
 }
