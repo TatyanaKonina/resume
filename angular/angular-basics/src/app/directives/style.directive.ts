c54333e200c2afacffd79581736cde7e4e4187c3
TatyanaKonina <tdkonina@edu.hse.ru>
2021-09-30 11:46:01 +0300

    Добавление событий

--- angular/angular-basics/src/app/directives/style.directive.ts
+++ angular/angular-basics/src/app/directives/style.directive.ts
@@ -1,11 +1,21 @@
-import { Directive, ElementRef, Renderer2 } from "@angular/core";
+import { Directive, ElementRef, HostListener, Renderer2 } from "@angular/core";
 
 @Directive({selector:'[appStyle]'})
 
 
 export class StyleDirective {
     constructor(private elRef: ElementRef,private render : Renderer2){
-        // elRef.nativeElement.style.color = 'red'
         this.render.setStyle(elRef.nativeElement,'color','blue')
+    }
+
+    @HostListener('click',['$event.target'])
+    onClick(event:Event){
+        console.log(event)
+    }
+    @HostListener('mouseenter') onEnter(){
+        this.render.setStyle(this.elRef.nativeElement,'color','red')
+    }
+    @HostListener('mouseleave') onLeave(){
+        this.render.setStyle(this.elRef.nativeElement,'color',null)
     }
 }
