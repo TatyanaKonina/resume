cbea5f739970cc8800b2aa40c3905072aae57b49
TatyanaKonina <tdkonina@edu.hse.ru>
2021-09-30 11:35:34 +0300

    init directive

--- angular/angular-basics/src/app/app.module.ts
+++ angular/angular-basics/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StyleDirective } from './directives/style.directive';
+import { Style2Directive } from './style2.directive';
+import { Style3Directive } from './directives/style3.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
+    StyleDirective,
+    Style2Directive,
+    Style3Directive
 
   ],
   imports: [
