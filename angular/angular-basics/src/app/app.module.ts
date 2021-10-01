37a873b9f079d3a88e83ffa7a77799bbbf611424
TatyanaKonina <tdkonina@edu.hse.ru>
2021-10-01 14:30:53 +0300

    generate pipe

--- angular/angular-basics/src/app/app.module.ts
+++ angular/angular-basics/src/app/app.module.ts
@@ -3,13 +3,13 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MultByPipe } from './pipes/mult-by.pipe';
-
+import { ExMarksPipe } from './pipes/ex-marks.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MultByPipe
+    ExMarksPipe,
+
   ],
   imports: [
     BrowserModule,
