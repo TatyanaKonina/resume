328a943d31804304b6eed52c54c5f769dbc1914f
TatyanaKonina <tdkonina@edu.hse.ru>
2021-10-10 10:04:11 +0300

    Динамические роуты

--- angular/angular-basics/src/app/app-routing.module.ts
+++ angular/angular-basics/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from "@angular/router";
 import { AboutComponent } from "./about/about.component";
 import { HomeComponent } from "./home/home.component";
 import { PostsComponent } from "./posts/posts.component";
+import { PostComponent } from "./post/post.component";
 
 const routes: Routes = [
     {path: '', component:HomeComponent},
     {path: 'about', component:AboutComponent},
     {path: 'posts', component:PostsComponent},
+    {path: 'posts/:id',component:PostComponent}
 ]
 
 @NgModule({
